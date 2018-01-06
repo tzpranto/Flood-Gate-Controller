2c0dcd2cd1c77695d50aff9bc519169f0c4552b4
tzpranto <md.toufikzaman@gmail.com>
2018-01-06 17:11:45 +0600

    Commit 2

--- Flood_Gate_Controller/Flood_Gate_Controller/Flood_Gate_Controller.c
+++ Flood_Gate_Controller/Flood_Gate_Controller/Flood_Gate_Controller.c
@@ -25,20 +25,74 @@
 #define WATER_MAX_HEIGHT 15
 #define WATER_TO_SONAR 4
 
+#define OFF_TIME 10
+#define ROTATION_TIME 200
+
 static volatile unsigned int pulse = 0;
 static volatile int pulse_reached = 0;
 
-typedef enum WATER_STATE {LOW, MEDIUM, HIGH, CRITICAL} water_state;
+typedef enum Height_State {LOW, MEDIUM, HIGH, CRITICAL} Height_State;
 
-water_state curr_water_state;
-water_state prev_water_state;
+Height_State water_state1;
+Height_State water_state2;
+Height_State water_state3;
+Height_State gate_state;
+
+void DC_Motor_Config() {
+	DDRA |= (1 << PINA1);
+	DDRA |= (1 << PINA2);
+	PORTA &= ~(1 << PINA1);
+	PORTA &= ~(1 << PINA2);
+}
+
+
+
+void DC_Motor_ClockwiseRotation() {
+	PORTA &= ~(1 << PINA1);
+	PORTA |= (1 << PINA2);
+}
+
+void DC_Motor_AntiClockwiseRotation() {
+	PORTA |= (1 << PINA1);
+	PORTA &= ~(1 << PINA2);
+}
+
+
+void DC_Motor_Off() {
+	PORTA &= ~(1 << PINA1);
+	PORTA &= ~(1 << PINA2);
+}
+
+
+
+void control_DC_Motor() {
+	if(water_state1 == water_state2 && water_state2 == water_state3 && water_state1 != gate_state) {
+		DC_Motor_Off();
+		_delay_ms(OFF_TIME);
+		if(water_state1 < gate_state) {
+			DC_Motor_ClockwiseRotation();
+			if(gate_state - water_state1 == 1) _delay_ms(ROTATION_TIME);
+			else if(gate_state - water_state1 == 2) _delay_ms(2*ROTATION_TIME);
+			else if(gate_state - water_state1 == 3) _delay_ms(3*ROTATION_TIME);
+		}
+		else {
+			DC_Motor_AntiClockwiseRotation();
+			if(gate_state - water_state1 == -1) _delay_ms(ROTATION_TIME);
+			else if(gate_state - water_state1 == -2) _delay_ms(2*ROTATION_TIME);
+			else if(gate_state - water_state1 == -3) _delay_ms(3*ROTATION_TIME);
+		}
+		gate_state = water_state1;
+		DC_Motor_Off();
+	}
+}
+
 
 /********
  * The higher the distance from sonar to water, lower the water level.
  * So when distance is highest get_water_state(int) returns LOW,
  * when distance is lowest it returns HIGH
  ********/
-water_state get_water_state(double height) {
+Height_State get_water_state(double height) {
 	height -= WATER_TO_SONAR;
 	if(height < 0) height = 0; 
 	if(height <= WATER_MAX_HEIGHT/4.0 && height >= 0) return CRITICAL;
@@ -47,7 +101,11 @@ water_state get_water_state(double height) {
 	return LOW;
 }
 
-
+void state_tracker(Height_State state) {
+	water_state1 = water_state2;
+	water_state2 = water_state3;
+	water_state3 = state;
+}
 
 ISR(INT0_vect)
 {
@@ -71,6 +129,7 @@ ISR(INT0_vect)
 
 int main(void)
 {
+	water_state1 = water_state2 = water_state3 = gate_state = LOW;
     DDRD = 0b11111011;
     DDRC = 0xFF;
 	DDRA |= 0x01;
@@ -94,25 +153,27 @@ int main(void)
 	Lcd4_Set_Cursor(2, 0);
 	Lcd4_Write_String("Distance: ");
 	
+	DC_Motor_Config();
 	while(1) {
-		PORTD|=(1<<PIND0);
+		PORTD |= (1<<PIND0);
 		_delay_us(15);
-		PORTD &=~(1<<PIND0);
+		PORTD &= ~(1<<PIND0);
 		while(pulse_reached == 0);
 		sonar_reading = (pulse*174.0)/10000;
 		dtostrf(sonar_reading, 6, 2, sonar_reading_display);
 		Lcd4_Set_Cursor(2, 10);
 		Lcd4_Write_String(sonar_reading_display);
 		Lcd4_Set_Cursor(1, 7);
-		if(get_water_state(sonar_reading) == LOW) {
+		Height_State currentState = get_water_state(sonar_reading);
+		if(currentState == LOW) {
 			Lcd4_Write_String("LOW      ");
 			PORTA &= ~(1<<PINA0);
 		}
-		else if(get_water_state(sonar_reading) == MEDIUM) {
+		else if(currentState == MEDIUM) {
 			Lcd4_Write_String("MEDIUM   ");
 			PORTA &= ~(1<<PINA0);
 		}
-		else if(get_water_state(sonar_reading) == HIGH) {
+		else if(currentState == HIGH) {
 			Lcd4_Write_String("HIGH     ");
 			PORTA &= ~(1<<PINA0);
 		}
@@ -120,9 +181,9 @@ int main(void)
 			Lcd4_Write_String("CRITICAL ");
 			PORTA |= (1<<PINA0);
 		}
+		state_tracker(currentState);
+		control_DC_Motor();
 		_delay_ms(100);
 	}
-	
-    
 }
 
